005aed78e7415569998deabb1e220e75013439fc
Prashanth S <92350320+prashanthrddy13@users.noreply.github.com>
2021-10-16 19:37:42 +0530

    Add files via upload

---
+++ Flower.java
@@ -0,0 +1,14 @@
+Class Flower{
+String name;
+String colour;
+int number of petals;
+void shedfragrance(){
+System.out.println(name +"is shedding fragrance..!");
+}
+void bloom(){
+Sytem.out.println(name +"is blooming..!");
+}
+public static void main(String args[]){
+flower;
+}
+}
